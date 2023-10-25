c1e88be4efb0a2af448928c0f373b4d6b2b33dd8
hyeonmi <pilotfish22@gmail.com>
2023-10-25 15:04:18 +0900

    product 서버 주소 적용

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,1 +1,1 @@
-export const API_URL = "http://localhost:8080"
+export const API_URL = process.env.NODE_ENV === "production" ? "https://study-grab-market-server22.fly.dev" : "http://localhost:8080";
